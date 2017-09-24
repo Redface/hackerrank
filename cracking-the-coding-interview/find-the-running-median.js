bdfeae6b10807a86f7c7606ebd3164efcbb0b821
redface <redface0512@gmail.com>
2017-09-25 00:20:00 +0900

    heap-sort applied

--- cracking-the-coding-interview/find-the-running-median.js
+++ cracking-the-coding-interview/find-the-running-median.js
@@ -26,22 +26,64 @@ function validateNumberOfValue(numberOfValue) {
 function validateValue(val) {
   assert(0 <= val && val <= Math.pow(10, 5), 'value is out of range');
 }
+
 function isOddLength(arr) {
   return arr.length % 2 !== 0;
 }
+function getIndex(length) {
+  return Number.parseInt((((length + 1) / 2) - 1));
+}
+
 function getOddMedian(arr) {
-  const length = arr.length
-  return length === 1 ? arr[0] : arr[length / 2];
+  return arr[getIndex(arr.length)];
 }
 function getEvenMedian(arr) {
-  const medianIndex = arr.length / 2;
+  const medianIndex = getIndex(arr.length);
   const beforeMedianIndex = medianIndex, afterMedianIndex = medianIndex + 1;
   return (arr[beforeMedianIndex] + arr[afterMedianIndex]) / 2;
 }
-function getMedian(arr) {
-  if (isOddLength(arr)) return Number.parseFloat(getOddMedian(arr)).toFixed(1);
-  else return Number.parseFloat(getEvenMedian(arr)).toFixed(1);
+
+function swap(arr, firstIndex, secondIndex) {
+  let targetArr = JSON.parse(JSON.stringify(arr));
+  let tmp = targetArr[firstIndex];
+  targetArr[firstIndex] = targetArr[secondIndex];
+  targetArr[secondIndex] = tmp;
+  return targetArr;
 }
+function heapify(arr, n, i) {
+  let largest = i; // init as root
+  const leftChild = 2 * i + 1, rightChild = 2 * i + 2;
+  let targetArr = JSON.parse(JSON.stringify(arr));
+
+  if (leftChild < n && arr[leftChild] > arr[largest]) largest = leftChild;
+  if (rightChild < n && arr[rightChild] > arr[largest]) largest = rightChild;
+
+  if (largest !== i) {
+    targetArr = swap(targetArr, i, largest);
+    targetArr = heapify(targetArr, n, largest);
+  }
+
+  return targetArr;
+
+}
+function heapSort(arr, n) {
+  let targetArr = JSON.parse(JSON.stringify(arr));
+  for(let i = 0; i < n; i++) targetArr = heapify(targetArr, n, i);
+
+  for(let i = n - 1; i >= 0; i--) {
+    const FIRST = 0, LAST = i;
+    targetArr = swap(targetArr, FIRST, LAST);
+    targetArr = heapify(targetArr, i, 0);
+  }
+  return targetArr;
+}
+
+function getMedian(arr) {
+  const sortedArr = heapSort(arr);
+  if (isOddLength(sortedArr)) return Number.parseFloat(getOddMedian(sortedArr)).toFixed(1);
+  else return Number.parseFloat(getEvenMedian(sortedArr)).toFixed(1);
+}
+
 function main() {
   var n = parseInt(readLine());
   var a = [];
@@ -49,7 +91,6 @@ function main() {
   for (var a_i = 0; a_i < n; a_i++) {
     a[a_i] = parseInt(readLine());
     validateValue(a[a_i]);
-    console.log(getMedian(a.sort()));
+    console.log(getMedian(a));
   }
-
 }

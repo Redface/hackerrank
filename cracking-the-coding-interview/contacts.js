e32f53c6772a6efffd9fce20f0efbfe82c11819b
redface <redface0512@gmail.com>
2017-11-07 00:23:52 +0900

    contact completed

--- cracking-the-coding-interview/contacts.js
+++ cracking-the-coding-interview/contacts.js
@@ -1,5 +1,3 @@
-// still in process
-
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
@@ -34,65 +32,39 @@ function validateContact(contact) {
   assert(1 <= length && length <= 21, 'out of contact char range');
 }
 
-function validateNameDuplication(trueCount) {
-  assert(trueCount !== 0, 'duplicated word');
-}
+class ContactObj {
 
-function addName(contact) {
-  let trueCount = 0;
-  for (let i = 0, length = contact.length; i < length; i++) {
-    const char = contact[i];
-    if (!trees[i]) trees.push(Object.assign({}, alphabets));
-    if (trees[i][char] === false) {
-      trees[i][char] = true;
-      trueCount++;
+  constructor() {
+    this.contactObject = {};
+    this.validateNameDuplication = (trueCount) => {
+      assert(trueCount !== 0, 'duplicated word');
     }
   }
-  validateNameDuplication(trueCount);
+
+  add(word) {
+    let increamentCount = 0;
+    for (let i = 1; i < word.length + 1; i++) {
+      const el = word.substring(0, i);
+      const prevCount = this.contactObject[el];
+      this.contactObject[el] = this.contactObject[el] ? this.contactObject[el] + 1 : 1;
+
+      if (prevCount !== this.contactObject[el]) increamentCount++;
+    }
+    this.validateNameDuplication(increamentCount);
+  }
+
+  find(word) {
+    const count = this.contactObject[word];
+    return this.contactObject[word] ? count : 0;
+  }
+
 }
 
-function isTrueCount(trees, contact, treeIndex, contactIndex) {
-  if (j === 0 && trees[treeIndex][contact[contactIndex]]) return true;
-  else if (contactIndex > 0 && (trees[treeIndex][contact[contactIndex - 1]] && trees[treeIndex][contact[contactIndex]])) return true;
-  return false;
-}
-function printHowManyContains(contact) {
-  const contactLength = contact.length;
-  // for (let i = 0, length = trees.length; i < length; i++) {
-  //   let trueCount = 0;
-  //   for (let j = 0; j < contactLength; j++) {
-  //     if (j === 0 && trees[i][contact[j]]) trueCount++;
-  //     else if (j > 0 && (trees[i][contact[j - 1]] && trees[i][contact[j]])) trueCount++;
-  //   }
-  // }
-
-  let i = 0, treeLength;
-  do {
-    let trueCount = 0;
-    let j = 0;
-    do {
-      if (isTrueCount(trees, contact, i, j)) {
-        trueCount++;
-      } else {
-        trueCount = 0;
-        i = 0;
-        j = 0
-      }
-
-      j++
-    } while (j < contactLength);
-    if (trueCount === contactLength) console.log(trueCount);
-    i++;
-  } while (i < treeLength)
-}
-
-var trees = [];
-var alphabets = {};
 function main() {
-  for (let i = 97, length = 122; i <= length; i++) alphabets[String.fromCharCode(i)] = false;
 
   var n = parseInt(readLine());
-  // for (let i = 0; i < n; i++) trees.push(Object.assign({}, alphabets));
+  const contactObj = new ContactObj();
+
   for (var a0 = 0; a0 < n; a0++) {
     var op_temp = readLine().split(' ');
     var op = op_temp[0]; // operation
@@ -101,7 +73,7 @@ function main() {
     validateOp(op);
     validateContact(contact);
 
-    if (op === ADD) addName(contact);
-    else if (op === FIND) printHowManyContains(contact);
+    if (op === ADD) contactObj.add(contact);
+    else if (op === FIND) console.log(contactObj.find(contact));
   }
 }
